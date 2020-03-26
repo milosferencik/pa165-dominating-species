4437d1ef1dbadcb4f9fab9605582a9a2d8f9fa68
Katarina Matusova <xmatuso1@fi.muni.cz>
2020-03-26 12:47:18 +0100

    Implement entity Animal and dao Animal

--- src/main/java/dao/implementations/AnimalImpl.java
+++ src/main/java/dao/implementations/AnimalImpl.java
@@ -1,31 +1,51 @@
 package dao.implementations;
 
 import dao.entities.Animal;
+import dao.entities.Environment;
+import dao.entities.User;
 import dao.interfaces.AnimalDao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
- * Created by Kostka on 23/03/2020.
+ * Created by Matusova on 26/03/2020.
  */
 public class AnimalImpl implements AnimalDao {
-    public void createAnimal(Animal animal) {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void createAnimal(Animal animal) {
+        if (animal == null) {
+            throw new IllegalArgumentException("Animal cannot be null");
+        }
+        entityManager.persist(animal);
     }
 
     public List<Animal> getAllAnimals() {
-        return null;
+        return entityManager.createQuery("SELECT a FROM Animal a", Animal.class).getResultList();
     }
 
     public Animal getAnimal(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+        return entityManager.find(Animal.class, id);
     }
 
     public void updateAnimal(Animal animal) {
-
+        if (animal == null) {
+            throw new IllegalArgumentException("Animal cannot be null");
+        }
+        entityManager.merge(animal);
     }
 
     public void deleteAnimal(Animal animal) {
-
+        if (animal == null) {
+            throw new IllegalArgumentException("Animal cannot be null");
+        }
+        entityManager.remove(animal);
     }
 }
