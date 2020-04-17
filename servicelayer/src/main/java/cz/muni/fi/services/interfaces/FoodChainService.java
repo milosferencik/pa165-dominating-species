57e286f39188368110c1a92a70aec18747649c67
Milos Ferencik <ferencikmilos@gmail.com>
2020-04-17 21:41:17 +0200

    create FoodChainService interface with CRUD functions

--- servicelayer/src/main/java/cz/muni/fi/services/interfaces/FoodChainService.java
+++ servicelayer/src/main/java/cz/muni/fi/services/interfaces/FoodChainService.java
@@ -1,4 +1,41 @@
 package cz.muni.fi.services.interfaces;
 
+import dao.entities.FoodChain;
+
+import java.util.List;
+
+/**
+ * Created by Milos Ferencik on 17/04/2020.
+ */
 public interface FoodChainService {
+    /**
+     * Create new entity in the database
+     * @param foodChain to be created
+     */
+    void createFoodChain(FoodChain foodChain);
+
+    /**
+     * Finds all entities in the database
+     * @return List of all FoodChains
+     */
+    List<FoodChain> getAllFoodChains();
+
+    /**
+     * Find entity with given id in the database
+     * @param id of FoodChain
+     * @return FoodChain or null, if there is no FoodChain with given id
+     */
+    FoodChain getFoodChain(Long id);
+
+    /**
+     * Update entity in the database
+     * @param foodChain to be updated
+     */
+    void updateFoodChain(FoodChain foodChain);
+
+    /**
+     * Delete entity from the database
+     * @param foodChain to be deleted
+     */
+    void deleteFoodChain(FoodChain foodChain);
 }
