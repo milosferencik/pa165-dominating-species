648ac4ac3e929b0d126ff3d035f842c9f4acecf3
Katarina Matusova <xmatuso1@fi.muni.cz>
2020-04-18 14:13:35 +0200

    Added EnvironmentService and EnvironmentServiceImpl classes

---
+++ servicelayer/src/main/java/cz/muni/fi/services/implementations/EnvironmentServiceImpl.java
@@ -0,0 +1,4 @@
+package cz.muni.fi.services.implementations;
+
+public class EnvironmentServiceImpl {
+}
---
+++ servicelayer/src/main/java/cz/muni/fi/services/interfaces/EnvironmentService.java
@@ -0,0 +1,4 @@
+package cz.muni.fi.services.interfaces;
+
+public interface EnvironmentService {
+}
