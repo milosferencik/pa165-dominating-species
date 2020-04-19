3c38b813c78c6c19cbc3a1f07c93aa8482783a5c
Katarina Matusova <xmatuso1@fi.muni.cz>
2020-04-19 10:24:53 +0200

    Added exception handling in UserService and UserServiceImpl

--- servicelayer/src/main/java/cz/muni/fi/services/implementations/UserServiceImpl.java
+++ servicelayer/src/main/java/cz/muni/fi/services/implementations/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package cz.muni.fi.services.implementations;
 
+import cz.muni.fi.services.exceptions.ServiceDataAccessException;
 import cz.muni.fi.services.interfaces.UserService;
 import dao.entities.User;
 import dao.interfaces.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
@@ -12,38 +15,62 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
 
     @Override
-    public void createUser(User user) {
-        userDao.createUser(user);
+    public void createUser(User user) throws DataAccessException {
+        try {
+            userDao.createUser(user);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not create user.", ex);
+        }
+
     }
 
     @Override
-    public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+    public List<User> getAllUsers() throws DataAccessException {
+        try {
+            return userDao.getAllUsers();
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not get all users.", ex);
+        }
     }
 
     @Override
-    public User getUser(Long id) {
-        return userDao.getUser(id);
+    public User getUser(Long id) throws DataAccessException {
+        try {
+            return userDao.getUser(id);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not get user.", ex);
+        }
     }
 
     @Override
-    public void updateUser(User user) {
-        userDao.updateUser(user);
+    public void updateUser(User user) throws DataAccessException {
+        try {
+            userDao.updateUser(user);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not update user.", ex);
+        }
     }
 
     @Override
-    public void deleteUser(User user) {
-        userDao.deleteUser(user);
+    public void deleteUser(User user) throws DataAccessException {
+        try {
+            userDao.deleteUser(user);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not delete user.", ex);
+        }
     }
 
     @Override
-    public User getUserByEmail(String email) {
-         return userDao.getUserByEmail(email);
+    public User getUserByEmail(String email) throws DataAccessException {
+        try {
+            return userDao.getUserByEmail(email);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not get user by email.", ex);
+        }
     }
 
     @Override
-    public boolean isAdmin(User user) {
+    public boolean isAdmin(User user) throws DataAccessException {
         return getUser(user.getId()).isAdmin();
     }
-
 }
