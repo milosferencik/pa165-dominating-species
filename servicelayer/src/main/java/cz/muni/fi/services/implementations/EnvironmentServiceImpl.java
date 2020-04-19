25da732e9e9c7da2f66197a2d2f73df13e2f1751
Katarina Matusova <xmatuso1@fi.muni.cz>
2020-04-19 10:30:01 +0200

    Added exception handling in EnvironmentService and EnvironmentServiceImpl

--- servicelayer/src/main/java/cz/muni/fi/services/implementations/EnvironmentServiceImpl.java
+++ servicelayer/src/main/java/cz/muni/fi/services/implementations/EnvironmentServiceImpl.java
@@ -1,11 +1,12 @@
 package cz.muni.fi.services.implementations;
 
+import cz.muni.fi.services.exceptions.ServiceDataAccessException;
 import cz.muni.fi.services.interfaces.EnvironmentService;
 import dao.entities.Environment;
 import dao.interfaces.EnvironmentDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 /**
@@ -18,27 +19,47 @@ public class EnvironmentServiceImpl implements EnvironmentService {
     private EnvironmentDao environmentDao;
 
     @Override
-    public void createEnvironment(Environment environment) {
+    public void createEnvironment(Environment environment) throws DataAccessException {
+        try {
             environmentDao.createEnvironment(environment);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not create environment.", ex);
+        }
     }
 
     @Override
-    public List<Environment> getAllEnvironments() {
-        return environmentDao.getAllEnvironments();
+    public List<Environment> getAllEnvironments() throws DataAccessException {
+        try {
+            return environmentDao.getAllEnvironments();
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not get all environments.", ex);
+        }
     }
 
     @Override
-    public Environment getEnvironment(Long id) {
-        return environmentDao.getEnvironment(id);
+    public Environment getEnvironment(Long id) throws DataAccessException {
+        try {
+            return environmentDao.getEnvironment(id);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not get all environments.", ex);
+        }
     }
 
     @Override
-    public void updateEnvironment(Environment environment) {
-        environmentDao.updateEnvironment(environment);
+    public void updateEnvironment(Environment environment) throws DataAccessException {
+        try {
+            environmentDao.updateEnvironment(environment);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not update environment.", ex);
+        }
     }
 
     @Override
-    public void deleteEnvironment(Environment environment) {
-        environmentDao.deleteEnvironment(environment);
+    public void deleteEnvironment(Environment environment) throws DataAccessException {
+        try {
+            environmentDao.deleteEnvironment(environment);
+        } catch (Throwable ex) {
+            throw new ServiceDataAccessException("Could not delete environment.", ex);
+        }
     }
 }
