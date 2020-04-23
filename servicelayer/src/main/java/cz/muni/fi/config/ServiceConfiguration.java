3400809c5802191eee8f3ce9a25e80cf2972c903
Milos Ferencik <ferencikmilos@gmail.com>
2020-04-23 10:26:14 +0200

    create Bean mapping, userDto and packege for facades

--- servicelayer/src/main/java/cz/muni/fi/config/ServiceConfiguration.java
+++ servicelayer/src/main/java/cz/muni/fi/config/ServiceConfiguration.java
@@ -1,6 +1,12 @@
 package cz.muni.fi.config;
 
+import cz.muni.fi.dto.UserDTO;
 import dao.config.MainConfiguration;
+import dao.entities.User;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,4 +15,18 @@ import org.springframework.context.annotation.Import;
 @Import(MainConfiguration.class)
 @ComponentScan(basePackages = "cz.muni.fi")
 public class ServiceConfiguration {
+
+    @Bean
+    public Mapper dozer(){
+        DozerBeanMapper dozer = new DozerBeanMapper();
+        dozer.addMapping(new DozerCustomConfig());
+        return dozer;
+    }
+
+    public class DozerCustomConfig extends BeanMappingBuilder {
+        @Override
+        protected void configure() {
+            mapping(User.class, UserDTO.class);
+        }
+    }
 }
