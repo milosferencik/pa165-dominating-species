9c0a98275e5e90fe1c1532f557589d1cfe8ba722
Katarina Matusova <xmatuso1@fi.muni.cz>
2020-04-24 22:12:58 +0200

    Added annotations

--- servicelayer/src/main/java/cz/muni/fi/facades/FoodChainFacadeImpl.java
+++ servicelayer/src/main/java/cz/muni/fi/facades/FoodChainFacadeImpl.java
@@ -11,12 +11,16 @@ import dao.entities.Animal;
 import dao.entities.AnimalInFoodChain;
 import dao.entities.FoodChain;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * @author Katarina Matusova
  */
+@Service
+@Transactional
 public class FoodChainFacadeImpl implements FoodChainFacade {
 
     @Autowired
