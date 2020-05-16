2c667988831a5894b3e3bf58c35c858a45d2f932
Petr Kostka <kostkpe3@fel.cvut.cz>
2020-05-16 11:09:49 +0200

    Basic crud in Spring mvc

--- spring-mvc/src/main/java/cz/muni/fi/mvc/controllers/FoodChainController.java
+++ spring-mvc/src/main/java/cz/muni/fi/mvc/controllers/FoodChainController.java
@@ -1,16 +1,29 @@
 package cz.muni.fi.mvc.controllers;
 
+import cz.muni.fi.dto.EnvironmentCreateDTO;
 import cz.muni.fi.dto.EnvironmentDTO;
+import cz.muni.fi.dto.FoodChainCreateDTO;
 import cz.muni.fi.dto.FoodChainDTO;
 import cz.muni.fi.facades.FoodChainFacade;
+import cz.muni.fi.mvc.validators.EnvironmentCreateDtoValidator;
+import cz.muni.fi.mvc.validators.EnvironmentUpdateDtoValidator;
+import cz.muni.fi.mvc.validators.FoodChainCreateDtoValidator;
+import cz.muni.fi.mvc.validators.FoodChainUpdateDtoValidator;
+import dao.entities.FoodChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 /**
  * @author Petr Kostka
@@ -21,6 +34,8 @@ public class FoodChainController {
 
     @Autowired
     private FoodChainFacade foodChainFacade;
+
+    final static Logger log = LoggerFactory.getLogger(FoodChainController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String list(Model model) {
@@ -38,5 +53,53 @@ public class FoodChainController {
 
         redirectAttributes.addFlashAttribute("alert_success", "FoodChain with id" + foodChainDTO.getId() + " was successfully deleted");
         return "redirect:" + uriBuilder.path("/foodChain/list").toUriString();
+    }
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        if (binder.getTarget() instanceof FoodChainCreateDTO) {
+            binder.addValidators(new FoodChainCreateDtoValidator());
+        }
+        if (binder.getTarget() instanceof FoodChainDTO) {
+            binder.addValidators(new FoodChainUpdateDtoValidator());
+        }
+    }
+
+    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
+    public String detail(@PathVariable long id, Model model) {
+        log.debug("detail({})", id);
+        model.addAttribute("foodChain", foodChainFacade.getFoodChainById(id));
+        return "foodChain/detail";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create(Model model) {
+        log.debug("create()");
+        model.addAttribute("foodChainCreate", new FoodChainCreateDTO());
+        return "foodChain/create";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("foodChainCreate") FoodChainCreateDTO foodChainCreateDTO,
+                         BindingResult bindingResult,
+                         Model model,
+                         RedirectAttributes redirectAttributes,
+                         UriComponentsBuilder urisBuilder) {
+        log.debug("create(foodChainCreate={})", foodChainCreateDTO);
+        if (bindingResult.hasErrors()) {
+            for (ObjectError ge : bindingResult.getGlobalErrors()) {
+                log.trace("ObjectError: {}", ge);
+            }
+            for (FieldError fe: bindingResult.getFieldErrors()) {
+                model.addAttribute(fe.getField() + "_error", true);
+                log.trace("FieldError: {}", fe);
+            }
+            return "foodChain/create";
+        }
+
+        Long id = foodChainFacade.createFoodChain(foodChainCreateDTO);
+
+        redirectAttributes.addFlashAttribute("alert_success", "FoodChain " + id + " was created successfully");
+        return "redirect:" + urisBuilder.path("/foodChain/detail/{id}").buildAndExpand(id).encode().toUriString();
     }
 }
