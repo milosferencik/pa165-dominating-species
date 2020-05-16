841ea0f58c980a252e6b964d0a6cddc9680488b0
Milos Ferencik <ferencikmilos@gmail.com>
2020-05-16 17:40:29 +0200

    throw ResourceAlreadyExistingException in rest

--- rest/src/main/java/cz/muni/fi/rest/exceptions/ResourceAlreadyExistingException.java
+++ rest/src/main/java/cz/muni/fi/rest/exceptions/ResourceAlreadyExistingException.java
@@ -3,6 +3,6 @@ package cz.muni.fi.rest.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason="The resource already exists")
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY, reason="The resource with name already exists")
 public class ResourceAlreadyExistingException extends RuntimeException {
 }
