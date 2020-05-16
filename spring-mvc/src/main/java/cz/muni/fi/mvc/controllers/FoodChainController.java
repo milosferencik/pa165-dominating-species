ecbb0bcd8be516edc9902ee24f4da089654f9925
Petr Kostka <kostkpe3@fel.cvut.cz>
2020-05-16 09:16:28 +0200

    Updated  basic controller for REST for FoodChain and added to spring mvc

--- spring-mvc/src/main/java/cz/muni/fi/mvc/controllers/FoodChainController.java
+++ spring-mvc/src/main/java/cz/muni/fi/mvc/controllers/FoodChainController.java
@@ -1,11 +1,16 @@
 package cz.muni.fi.mvc.controllers;
 
+import cz.muni.fi.dto.EnvironmentDTO;
+import cz.muni.fi.dto.FoodChainDTO;
 import cz.muni.fi.facades.FoodChainFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * @author Petr Kostka
@@ -20,6 +25,18 @@ public class FoodChainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("foodChains", foodChainFacade.getAllFoodChains());
-        return "foodchain/list";
+        return "foodChain/list";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    public String delete(@PathVariable long id,
+                         Model model,
+                         UriComponentsBuilder uriBuilder,
+                         RedirectAttributes redirectAttributes) {
+        FoodChainDTO foodChainDTO = foodChainFacade.getFoodChainById(id);
+        foodChainFacade.deleteFoodChain(id);
+
+        redirectAttributes.addFlashAttribute("alert_success", "FoodChain with id" + foodChainDTO.getId() + " was successfully deleted");
+        return "redirect:" + uriBuilder.path("/foodChain/list").toUriString();
     }
 }
