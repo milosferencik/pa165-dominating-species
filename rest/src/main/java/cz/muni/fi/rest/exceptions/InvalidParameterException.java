f584f4a8f82212a8cf017d62ab0f78a622ef95c5
Milos Ferencik <ferencikmilos@gmail.com>
2020-05-16 16:13:37 +0200

    add exceptions in rest

---
+++ rest/src/main/java/cz/muni/fi/rest/exceptions/InvalidParameterException.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
+public class InvalidParameterException extends RuntimeException {
+}
